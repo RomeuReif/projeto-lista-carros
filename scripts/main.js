0025db162b882b0a0f3b2c1a6b7a95ed2d34def8
RomeuReif <romeureif10@gmail.com>
2023-12-17 16:31:04 -0800

    criado um evento de click nos buttons das marcas para ativar classe

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
-function pegaDados(){
-    
-}
+const listaMarcas = document.getElementById("lista-marcas");
+const buttonsMarcas = listaMarcas.querySelectorAll("button");
+
+
+// evento de click de quando clicar no botao de marcas e ativar
+buttonsMarcas.forEach(button =>{
+    button.addEventListener("click", function(){
+        // remove todas as classes active
+        buttonsMarcas.forEach(btn => btn.classList.remove("active"));
+        // adiciona a classe no botao clicado
+        button.classList.toggle("active");
+    })
+});
