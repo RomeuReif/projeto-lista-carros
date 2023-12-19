a467983632bd2af78743eb4654c2e5b525e679bf
RomeuReif <romeureif10@gmail.com>
2023-12-18 17:35:15 -0800

    desenvolvido o card de carros e desenvolvido o script para mostrar o container

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
 const listaMarcas = document.getElementById("lista-marcas");
 const buttonsMarcas = listaMarcas.querySelectorAll("button");
+const listaCarros = document.getElementById("lista-carros")
 
 
 // evento de click de quando clicar no botao de marcas e ativar
 buttonsMarcas.forEach(button =>{
     button.addEventListener("click", function(){
+        const marcaID = button.parentNode.id; // pega o id do button
+        const marcaContainer = document.getElementById(marcaID + '-container');
+
+        // adicionando a class hide para ocultar todos os container dos carros
+        listaCarros.querySelectorAll(".carros-container").forEach(container => {
+            container.classList.add("hide");
+        });
+
+        // removendo a classe hide para mostrar o container que foi clicado no button 
+        marcaContainer.classList.remove("hide");
+
+
         // remove todas as classes active
         buttonsMarcas.forEach(btn => btn.classList.remove("active"));
         // adiciona a classe no botao clicado
